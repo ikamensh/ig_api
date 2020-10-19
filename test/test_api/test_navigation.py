c34d66a90b2f8f2a637f343c401c12c0cf5f1dde
ikkamens <ikkamens@amazon.com>
2020-10-19 16:47:25 +0200

    added unit test

--- test/test_api/test_navigation.py
+++ test/test_api/test_navigation.py
@@ -12,3 +12,14 @@ def test_get_positions(ig_session):
     nxt = ids[0]
     ids, epics = ig_session.market_navigation(nxt)
     assert ids or epics
+
+def test_gets_to_market(ig_session):
+    """Tests that returned node ids can be used for further navigation. """
+    ids, epics = ig_session.market_navigation()
+    while ids:
+        nxt = ids[0]
+        ids, epics = ig_session.market_navigation(nxt)
+
+    epic = epics[0]
+    snap, instr = ig_session.market(epic)
+    
